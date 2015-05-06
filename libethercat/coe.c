140487817f42f8e8cfd131c5142624ed407a5d26
Robert Burger <robert.burger@dlr.de>
2015-05-06 08:22:00 +0000

    fixed error message on sdo read

--- libethercat/coe.c
+++ libethercat/coe.c
@@ -123,42 +123,6 @@ typedef struct {
     uint32_t abort_code;
 } PACKED ec_sdo_abort_request_t;
 
-///** SDO error list definition */
-//const ec_sdoerrorlist_t ec_sdoerrorlist[] = {
-//   {0x00000000, "No error" },
-//   {0x05030000, "Toggle bit not changed" },
-//   {0x05040000, "SDO protocol timeout" },
-//   {0x05040001, "Client/Server command specifier not valid or unknown" },
-//   {0x05040005, "Out of memory" },
-//   {0x06010000, "Unsupported access to an object" },
-//   {0x06010001, "Attempt to read to a write only object" },
-//   {0x06010002, "Attempt to write to a read only object" },
-//   {0x06010003, "Subindex can not be written, SI0 must be 0 for write access" },
-//   {0x06010004, "SDO Complete access not supported for variable length objects" },
-//   {0x06010005, "Object length exceeds mailbox size" },
-//   {0x06010006, "Object mapped to RxPDO, SDO download blocked" },
-//   {0x06020000, "The object does not exist in the object directory" },
-//   {0x06040041, "The object can not be mapped into the PDO" },
-//   {0x06040042, "The number and length of the objects to be mapped would exceed the PDO length" },
-//   {0x06040043, "General parameter incompatibility reason" },
-//   {0x06040047, "General internal incompatibility in the device" },
-//   {0x06060000, "Access failed due to a hardware error" },
-//   {0x06070010, "Data type does not match, length of service parameter does not match" },
-//   {0x06070012, "Data type does not match, length of service parameter too high" },
-//   {0x06070013, "Data type does not match, length of service parameter too low" },
-//   {0x06090011, "Subindex does not exist" },
-//   {0x06090030, "Value range of parameter exceeded (only for write access)" },
-//   {0x06090031, "Value of parameter written too high" },
-//   {0x06090032, "Value of parameter written too low" },
-//   {0x06090036, "Maximum value is less than minimum value" },
-//   {0x08000000, "General error" },
-//   {0x08000020, "Data cannot be transferred or stored to the application" },
-//   {0x08000021, "Data cannot be transferred or stored to the application because of local control" },
-//   {0x08000022, "Data cannot be transferred or stored to the application because of the present device state" },
-//   {0x08000023, "Object dictionary dynamic generation fails or no object dictionary is present" },
-//   {0xffffffff, "Unknown" }
-//};
-
 //! read coe sdo 
 /*!
  * \param pec pointer to ethercat master
@@ -222,10 +186,10 @@ int ec_coe_sdo_read(ec_t *pec, uint16_t slave, uint16_t index, uint8_t sub_index
         ec_sdo_abort_request_t *abort_buf = 
             (ec_sdo_abort_request_t *)(pec->slaves[slave].mbx_read.buf); 
 
-        ec_log(10, "ec_coe_sdo_write", "got sdo abort request on idx %#X, "
-            "subidx %d, abortcode %#X: %s\n", index, sub_index, abort_buf->abort_code);
+        ec_log(100, "ec_coe_sdo_write", "got sdo abort request on idx %#X, "
+            "subidx %d, abortcode %#X\n", index, sub_index, abort_buf->abort_code);
 
-        *abort_code = abort_code;
+        *abort_code = abort_buf->abort_code;
         *len = 0;
         return wkc;
     }
