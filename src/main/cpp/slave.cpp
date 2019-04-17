dd432d1564ccba87db2fae6d3fb07bbd744a86b9
Robert Burger <robert.burger@dlr.de>
2019-04-17 10:09:53 +0200

    trying to map eeprom txpdos to key_value

--- src/main/cpp/slave.cpp
+++ src/main/cpp/slave.cpp
@@ -714,6 +714,10 @@ void slave::post_state_transition(module_state_t from, module_state_t to) {
                             entry->entries[i].entry_index, entry->entries[i].sub_index);
                     _add_key(create_key_read_only<uint8_t>(this, prefix2 + "entry_name_idx",
                                 &entry->entries[i].entry_name_idx, "Name index in strings"));
+                    if (entry->entries[i].entry_name_idx < master_dev->pec->slaves[index].eeprom.strings_cnt)
+                        _add_key(create_key_read_only<uint8_t>(this, prefix2 + "entry_name",
+                            &master_dev->pec->slaves[index].eeprom.strings[i], "Name"));
+
                     _add_key(create_key_read_only<uint8_t>(this, prefix2 + "data_type",
                                 &entry->entries[i].data_type, "Data type"));
                     _add_key(create_key_read_only<uint8_t>(this, prefix2 + "bit_len",
