a4075852cd8b032844e66e2229805c0043023730
Robert Burger <robert.burger@dlr.de>
2019-07-02 07:52:36 +0200

    updated conanfile

--- conanfile.py
+++ conanfile.py
@@ -7,13 +7,7 @@ class MainProject(ConanFile):
     url = f"https://rmc-github.robotic.dlr.de/robotkernel/{name}"
     description = "robotkernel-5 is a modular, easy configurable hardware abstraction framework"
     settings = "os", "compiler", "build_type", "arch"
-    scm = {
-        "type": "git",
-        "url": "auto",
-        "revision": "auto",
-        "submodule": "recursive",
-    }
-
+    exports_sources = "src/*", "README.wiki", "project.properties", "module_ethercat.pc.in", "Makefile.am", "m4/*", "configure.ac", "LICENSE"
     generators = "pkg_config"
     requires = (
             "robotkernel/[~=5.0]@robotkernel/unstable",
