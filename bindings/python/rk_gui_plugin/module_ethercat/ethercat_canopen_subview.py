1eddffb472c557d486c9629fb28b9f62d2f9cab5
Robert Burger <robert.burger@dlr.de>
2020-02-27 16:38:45 +0100

    added gui plugin

---
+++ bindings/python/rk_gui_plugin/module_ethercat/ethercat_canopen_subview.py
@@ -0,0 +1,92 @@
+'''
+(C) Robert Burger <robert.burger@dlr.de>
+
+This file is part of Robotkernel-GUI.
+
+Robotkernel-GUI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Robotkernel-GUI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Robotkernel-GUI.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import os, gtk, gobject
+import helpers
+
+from service_provider_canopen_protocol import canopen_device
+from service_provider_canopen_protocol import canopen_protocol_view
+
+class ethercat_canopen_subview(helpers.builder_base):
+    def __init__(self, parent):
+        fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dual_list.ui')
+        helpers.builder_base.__init__(self, fn, 'main')
+
+        self.parent = parent
+        self.app = parent.app
+        self.init_gui()
+
+    def init_gui(self):
+        # treeviews
+        self.create_device_treeview()
+        hbox = gtk.HBox()
+        self.devices.add_with_viewport(hbox)
+        self.devices.get_child().set_shadow_type(gtk.SHADOW_NONE)
+        hbox.add(self.treeview_devices)
+        self.treeview_devices.connect("cursor-changed", self.on_treeview_devices_cursor_changed)
+
+        hbox = gtk.HBox()
+        self.canopen_view = canopen_protocol_view(self.parent, hbox)
+        self.values.add_with_viewport(hbox)
+        self.values.get_child().set_shadow_type(gtk.SHADOW_NONE)
+
+        self.main.show_all()
+
+    #CREATORS
+    def create_device_treeview(self):
+        self.liststore_devices = store = gtk.ListStore(str, str, gobject.TYPE_PYOBJECT) # name, data
+        self.treeview_devices = view = gtk.TreeView(store)
+        view.set_model(store)
+        view.insert_column(gtk.TreeViewColumn("Slave", gtk.CellRendererText(), text=0), -1)
+        view.insert_column(gtk.TreeViewColumn("Name", gtk.CellRendererText(), text=1), -1)
+        view.connect("cursor-changed", self.on_treeview_devices_cursor_changed)
+        store.set_sort_column_id(0, 0)
+
+    def fill_device_treeview(self, module):
+        self.liststore_devices.clear()
+
+        #initially fill all devices found by ln
+        for s in module.childs:
+            def add(prefix, sort_key, devname):
+                try:
+                    ethercat_device = canopen_device(prefix, self.app, self.canopen_view, module.name, devname)
+                    indices = ethercat_device.list_dictionary()
+                    name = 'N/A'
+                    if 0x1008 in indices:
+                        name = ethercat_device.read_element(index=0x1008, sub_index=0).value
+
+                    self.liststore_devices.append( (sort_key, name, ethercat_device) )
+                except:
+                    import traceback
+                    print traceback.format_exc()
+                    pass
+
+            number = int(s.split('_')[-1])
+
+            for t in ['mailbox', 'eeprom']:
+                devname = '.'.join([s, t])
+                sort_key = '%s %d' % (t, number)
+                gobject.timeout_add(10, add, module.robotkernel_name, sort_key, devname)
+
+    #CALLBACKS
+    def on_treeview_devices_cursor_changed(self, widget):
+        model, iter = widget.get_selection().get_selected()
+        dev = model[iter][2] #device_id, device_name, pyobject
+        self.canopen_view.show(dev.modname, dev.devname)
+
--- conanfile.py
+++ conanfile.py
@@ -1,51 +1,23 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-import re, os
+from conans import tools, python_requires
+import os
 
-class MainProject(ConanFile):
+base = python_requires("conan_template/[~=5]@robotkernel/stable")
+
+class MainProject(base.RobotkernelConanFile):
     name = "module_ethercat"
-    license = "GPLv3"
-    author = "Robert Burger <robert.burger@dlr.de>"
-    url = f"https://rmc-github.robotic.dlr.de/robotkernel/{name}"
-    description = "robotkernel-5 is a modular, easy configurable hardware abstraction framework"
-    settings = "os", "compiler", "build_type", "arch"
+    description = "robotkernel-5 EtherCAT master module based on libethercat."
     exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
-    generators = "pkg_config"
     requires = (
-            "robotkernel/[~=5.0]@robotkernel/stable",
-            "service_provider_memory_inspection/[~=5.0]@robotkernel/stable",
-            "service_provider_canopen_protocol/[~=5.0]@robotkernel/stable",
-            "service_provider_key_value/[~=5.0]@robotkernel/stable",
-            "service_provider_sercos_protocol/[~=5.0]@robotkernel/stable",
-            "service_provider_file_protocol/[~=5.0]@robotkernel/stable",
-            "service_provider_process_data_inspection/[~=5.0]@robotkernel/stable",
+            "robotkernel/[~=5]@robotkernel/stable",
+            "service_provider_memory_inspection/[~=5]@robotkernel/stable",
+            "service_provider_canopen_protocol/[~=5]@robotkernel/stable",
+            "service_provider_key_value/[~=5]@robotkernel/stable",
+            "service_provider_sercos_protocol/[~=5]@robotkernel/stable",
+            "service_provider_file_protocol/[~=5]@robotkernel/stable",
+            "service_provider_process_data_inspection/[~=5]@robotkernel/stable",
             "libethercat/[~=0.2]@common/stable" )
 
-    def source(self):
-        filedata = None
-        filename = "project.properties"
-        with open(filename, 'r') as f:
-            filedata = f.read()
-        with open(filename, 'w') as f:
-            f.write(re.sub("VERSION *=.*[^\n]", f"VERSION = {self.version}", filedata))
-
-    def build(self):
-        self.run("autoreconf -if")
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.libs=[]
-        autotools.include_paths=[]
-        autotools.library_paths=[]
-        if self.settings.build_type == "Debug":
-            autotools.flags = ["-O0", "-g"]
-        else:
-            autotools.flags = ["-O3"]
-        autotools.configure(configure_dir=".")
-        autotools.make()
-
-    def package(self):
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.install()
-
     def package_info(self):
-        self.cpp_info.includedirs = ['include']
-        self.cpp_info.bindirs = ['bin']
-        self.cpp_info.resdirs = ['share']
+        super(base.RobotkernelConanFile, self).package_info()
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "bindings/python/rk_gui_plugin"))
+
