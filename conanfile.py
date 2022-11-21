71493886b6caf1981e637d99aa445db99e7796ea
Robert Burger <robert.burger@dlr.de>
2022-11-21 18:08:59 +0100

    fix: using correct libethercat version

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class MainProject(ConanFile):
             "service_provider_sercos_protocol/[~=5]@robotkernel/stable",
             "service_provider_file_protocol/[~=5]@robotkernel/stable",
             "service_provider_process_data_inspection/[~=5]@robotkernel/stable",
-            "libethercat/[~0.4]@common/snapshot" ]
+            "libethercat/0.4.0-rx-no-wait@common/snapshot" ]
 
     options = {
             "max_slaves"                 : "ANY",
