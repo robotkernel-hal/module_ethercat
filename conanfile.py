d0c4b89343ee9e6e3aafdbbdbec42fd253b8a89a
Robert Burger <robert.burger@dlr.de>
2024-02-20 14:47:55 +0100

    change: using libethercat/0.5.1 stable

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class MainProject(ConanFile):
     url = "https://rmc-github.robotic.dlr.de/robotkernel/module_ethercat"
     exports_sources = ["*", "!.gitignore"]
     requires = [
-            "libethercat/[>=0.5.1]@common/unstable",
+            "libethercat/[>=0.5.1]@common/stable",
             "robotkernel/[~=5]@robotkernel/stable",
             "service_provider_memory_inspection/[~=5]@robotkernel/stable",
             "service_provider_canopen_protocol/[~=5]@robotkernel/stable",
