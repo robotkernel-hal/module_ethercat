d95f2c47548ff799fc2855e82e1a567de4835300
Robert Burger <robert.burger@dlr.de>
2025-08-01 13:02:22 +0200

    change: unified deb build action

--- debian/module-ethercat.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libosal package
-#
-0 4	* * *	root	[ -x /usr/bin/libosal_maintenance ] && /usr/bin/libosal_maintenance
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liblibosal 0.0.2 libosal (>> 0.0.2-0), libosal (<< 0.0.2-99)
