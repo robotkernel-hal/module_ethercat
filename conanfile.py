ee602b18b5fdf091f7c176addb94bff179fe0a98
Robert Burger <robert.burger@dlr.de>
2026-04-22 09:27:38 +0200

    fix: using conan_template 6

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[^5.0.6]@robotkernel/stable"
+    python_requires = "conan_template/[~6]@robotkernel/unstable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_ethercat"
